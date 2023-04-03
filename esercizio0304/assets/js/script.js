27e4feecc394301d3a6744bfac25c7b84fa7e185
fedoraingrosso <fedory91@gmail.com>
2023-04-03 22:07:44 +0200

    0304

---
+++ esercizio0304/assets/js/script.js
@@ -0,0 +1,7 @@
+alert("Primo esercizio");
+alert("Buona fortuna!");
+
+document.getElementById("titolo").innerHTML ="Iniziamo la nostra pratica";
+
+document.write("output scritto da Javascript");
+
---
+++ esercizio0304/assets/js/script2.js
@@ -0,0 +1,12 @@
+var nomeAnimale = "criceto";
+var NomeAnimale ="giraffa";
+var nomeanimale = "leone";
+var nome_animale = 'pantera';
+
+
+console.log("criceto");
+console.log("giraffa");
+console.log("leone");
+
+
+document.write('<div id="container">' + nome_animale + 'scritto da Javascript</div>');
